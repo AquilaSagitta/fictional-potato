f5c8f4d99523405f0eb5fb12f6ab1629d28275d7
AquilaSagitta <blahutsurveys@gmail.com>
2016-05-27 21:36:20 -0400

    Updated src/back/server.js

--- src/back/server.js
+++ src/back/server.js
@@ -4,11 +4,11 @@ const express = require('express');
 
 // Constants
 const PORT = 8080;
-const SERVE_DIR = __dirname + "/front";
+const SERVE_DIR = __dirname + "/../front";
 
 // App
 const app = express();
-app.use(express.static(__dirname + '/../front'));
+app.use(express.static(SERVE_DIR));
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
